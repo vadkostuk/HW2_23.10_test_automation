e026b904fa6146f51f3f07328102fc7752ec63c8
vadkostuk <vadkostuk@gmail.com>
2020-10-20 17:55:19 +0300

    added a new branch in which added a method that solves the problems from the task3

--- src/Homework2/Main.java
+++ src/Homework2/Main.java
@@ -6,28 +6,32 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int x,r,a,b,c;
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введіть довжину отвору: x= ");
-        x=Integer.parseInt(br.readLine());
-        System.out.println("Введіть ширину отвору: r= ");
-        r=Integer.parseInt(br.readLine());
-        System.out.println("Введіть довжину цеглини: a= ");
-        a=Integer.parseInt(br.readLine());
-        System.out.println("Введіть ширину цеглини: b= ");
-        b=Integer.parseInt(br.readLine());
-        System.out.println("Введіть висоту цеглини: c= ");
-        c=Integer.parseInt(br.readLine());
-        BrickAndHole(x,r,a,b,c);
+        System.out.println("Завданя 1: \nВведіть радіус клумби:");
+        int radius = Integer.parseInt(br.readLine());
+        PerimeterAndArea(radius);
+        System.out.println("Завдання 2: \nWhat is your name?");
+        String name = br.readLine();
+        System.out.println("Where are you live "+ name+"?");
+        String address = br.readLine();
+        System.out.println("Nice to meet you "+name+" from "+address);
+        System.out.println("Завдання 3: \nВведіть 6 чисел:");
+        int c1 = Integer.parseInt(br.readLine());
+        int c2 = Integer.parseInt(br.readLine());
+        int c3 = Integer.parseInt(br.readLine());
+        int t1 = Integer.parseInt(br.readLine());
+        int t2 = Integer.parseInt(br.readLine());
+        int t3 = Integer.parseInt(br.readLine());
+        PhoneCalls(c1,c2,c3,t1,t2,t3);
+
     }
 
-    public static void BrickAndHole(int h_length, int h_width, int b_length, int b_width, int b_height){
-        if ( (b_length<=h_length && b_height<=h_width) || (b_length<=h_width && b_height<=h_length ) ||
-                (b_length<=h_length && b_width<=h_width) || (b_length<=h_width && b_width<=h_length) ||
-                (b_width<=h_length && b_height<=h_width) || (b_height<=h_length && b_width<=h_width)){
-            System.out.println("Цеглина може пройти через отвір");
-        }else{
-            System.out.println("Цеглина не пройде через отвір");
-        }
+    public static void PerimeterAndArea(int radius){
+        double perimeter = 2*Math.PI*radius;
+        double area =  Math.PI*radius*radius;
+        System.out.println("Відровідь:\nПериметр: "+perimeter+" Площа: "+area);
+    }
+    public static void PhoneCalls (int c1, int c2, int c3, int t1,int t2,int t3){
+        System.out.printf("Для першого:%d Для другого:%d Для третього:%d Для всіх разом:%d",c1*t1,c2*t2,c3*t3,c1*t1+c2*t2+c3*t3);
     }
 }
