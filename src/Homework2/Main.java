46d2fd690584ca756cfe9427370724d640b1b6f6
vadkostuk <vadkostuk@gmail.com>
2020-10-20 17:05:09 +0300

    added a new branch in which added a method that solves the problems from the second task

--- src/Homework2/Main.java
+++ src/Homework2/Main.java
@@ -1,33 +1,41 @@
 package Homework2;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int x,r,a,b,c;
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введіть довжину отвору: x= ");
-        x=Integer.parseInt(br.readLine());
-        System.out.println("Введіть ширину отвору: r= ");
-        r=Integer.parseInt(br.readLine());
-        System.out.println("Введіть довжину цеглини: a= ");
-        a=Integer.parseInt(br.readLine());
-        System.out.println("Введіть ширину цеглини: b= ");
-        b=Integer.parseInt(br.readLine());
-        System.out.println("Введіть висоту цеглини: c= ");
-        c=Integer.parseInt(br.readLine());
-        BrickAndHole(x,r,a,b,c);
-    }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введіть n:");
+        int n = Integer.parseInt(br.readLine());
+        task2_a(n);
+        task2_b(n);
+        task2_c(n);
+        task2_d(n);
+}
+ public static void task2_a(int n){
+     int squared = n*n;
+     String r = Integer.toString(squared);
+     if (r.indexOf('3')!=-1){
+         System.out.println("a) Цифра 3 присутня в числі "+squared);
+     }else{
+         System.out.println("a) Цифра 3 відсутня в числі "+squared);
+     }
+ }
 
-    public static void BrickAndHole(int h_length, int h_width, int b_length, int b_width, int b_height){
-        if ( (b_length<=h_length && b_height<=h_width) || (b_length<=h_width && b_height<=h_length ) ||
-                (b_length<=h_length && b_width<=h_width) || (b_length<=h_width && b_width<=h_length) ||
-                (b_width<=h_length && b_height<=h_width) || (b_height<=h_length && b_width<=h_width)){
-            System.out.println("Цеглина може пройти через отвір");
-        }else{
-            System.out.println("Цеглина не пройде через отвір");
-        }
-    }
+ public static void task2_b(int n){
+     System.out.println("б) "+new StringBuffer(Integer.toString(n)).reverse().toString());
+ }
+
+ public static void task2_c(int n){
+        String str = Integer.toString(n);
+         System.out.println("в) "+str.charAt(str.length()-1)+str.substring(1,str.length()-1)+str.charAt(0));
+
+ }
+ public static void task2_d(int n){
+        StringBuilder sb = new StringBuilder(Integer.toString(n));
+        System.out.println("г) "+sb.insert(0,'1').insert(sb.length(),'1'));
+ }
 }
